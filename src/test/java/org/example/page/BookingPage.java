e33c381484834bcfce42c2af93f4c1d2b9327476
Ovcharenko Oleksandr <ovcharenko.messor@gmail.com>
2022-07-29 11:53:21 +0200

    update clicking on the date button

--- src/test/java/org/example/page/BookingPage.java
+++ src/test/java/org/example/page/BookingPage.java
@@ -4,10 +4,8 @@ import lombok.AccessLevel;
 import lombok.SneakyThrows;
 import lombok.experimental.FieldDefaults;
 import org.example.util.WebDriverUtils;
-import org.openqa.selenium.By;
-import org.openqa.selenium.StaleElementReferenceException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -25,7 +23,7 @@ public class BookingPage {
 
     static By SEARCH_BOX = cssSelector("#ss");
     static By CALENDAR_BODY = cssSelector("#frm > div.xp__fieldset.js--sb-fieldset.accommodation > div.xp__dates.xp__group > div.xp-calendar > div");
-    static By CHECKIN_CALENDAR_BODY = By.xpath("//div[contains(@class, 'b-datepicker')][@data-mode='checkin']");
+    static By CHECKIN_CALENDAR_BODY = xpath("//div[contains(@class, 'b-datepicker')][@data-mode='checkin']");
     static By CHECK_IN_MONTH_YEAR = cssSelector("#frm > div.xp__fieldset.js--sb-fieldset.accommodation > div.xp__dates.xp__group > div.xp__dates-inner > div:nth-child(2) > div > div > div > div > div.sb-date-field__controls.sb-date-field__controls__ie-fix > div.sb-date-field__select.-month-year.js-date-field__part > select > option:nth-child(7)");
     //    static By CHECK_IN_DAY = cssSelector("#frm > div.xp__fieldset.js--sb-fieldset.accommodation > div.xp__dates.xp__group > div.xp__dates-inner > div:nth-child(2) > div > div > div > div > div.sb-date-field__controls.sb-date-field__controls__ie-fix > div.sb-date-field__select.-day.js-date-field__part.sb-date-field__select_disabled > select");
     static By CHECK_OUT_MONTH_YEAR = cssSelector("#frm > div.xp__fieldset.js--sb-fieldset.accommodation > div.xp__dates.xp__group > div.xp__dates-inner > div:nth-child(3) > div > div > div > div > div.sb-date-field__controls.sb-date-field__controls__ie-fix > div.sb-date-field__select.-month-year.js-date-field__part > select > option:nth-child(7)");
@@ -33,8 +31,9 @@ public class BookingPage {
 
     static By CHECKIN_MONTH = cssSelector("#frm > div.xp__fieldset.js--sb-fieldset.accommodation > div.xp__dates.xp__group > div.xp-calendar > div > div > div.bui-calendar__content > div:nth-child(1)");
 
-    static By CHECK_IN_DAY = xpath("//*[@id=\"frm\"]/div[1]/div[2]/div[2]/div/div/div[3]/div[2]/table/tbody/tr[1]/td[4]");
-    static By CHECK_OUT_DAY = xpath("//*[@id=\"frm\"]/div[1]/div[2]/div[2]/div/div/div[3]/div[2]/table/tbody/tr[5]/td[6]");
+    static By CHECK_IN_DAY = xpath("//*[contains(@data-date, '2022-12-01')]");
+    static By CHECK_OUT_DAY = xpath("//*[contains(@data-date, '2022-12-31')]");
+    static By CALENDAR_ROW = xpath("//div[contains(@class, 'bui-calendar__row')]");
 
     static By ATTRACTIONS_BUTTON = xpath("//*[@id=\"b2indexPage\"]/header/nav[2]/ul/li[4]/a");
     static By SEARCH_BUTTON = cssSelector("#frm > div.xp__fieldset.js--sb-fieldset.accommodation > div.xp__button > div.sb-searchbox-submit-col.-submit-button > button");
@@ -56,13 +55,10 @@ public class BookingPage {
     @SneakyThrows
     public void setDate() {
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(CHECKIN_CALENDAR_BODY)).click();
-        //TODO: Для Влада, если будешь читать утром) Этим корявым методом у меня получилось реализовать переключение месяцев
-        // и остановка на том что мне нужен, но дальше вылетает эксепшен когда я пытаюсь нажать на цифру
-        // Получается просто общараться к цифре как я это делаю в константах CHECK_IN_DAY и CHECK_OUT_DAY нельзя.
         selectDate();
     }
 
-    public void selectDate() {
+    private void selectDate() {
         WebElement button = webDriver.findElement(xpath("//*[contains(@data-bui-ref, 'calendar-next')]"));
         List<WebElement> monthYear;
 
@@ -71,8 +67,8 @@ public class BookingPage {
             if (!monthYear.get(0).getText().equals("December 2022")) {
                 button.click();
             } else {
-                webDriverUtils.waitAndClick(CHECK_IN_DAY);
-                webDriverUtils.waitAndClick(CHECK_OUT_DAY);
+                webDriverUtils.waitAndClick(xpath("//*[contains(@data-date, '2022-12-01')]"));
+                webDriverUtils.waitAndClick(xpath("//*[contains(@data-date, '2022-12-31')]"));
             }
         }
     }
